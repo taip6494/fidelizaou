d63fccaab5374ad4cd9f22c56dc7757750bb6cce
taip6494 <palisseauthomas@gmail.com>
2022-02-08 11:17:31 +0400

    version 3 - page connexion fini css

--- vues/form/form_conn/form_conn.php
+++ vues/form/form_conn/form_conn.php
@@ -1,4 +1,15 @@
-<body>
+<body class="form">
 <div class="container">
-  <h1>Hello</h1>
+
+<form>
+<center><img src="http://localhost/fidelizaou/images/logo4.png" width=250 height=150   class="logo"/></center>
+        <h3 class="titre_conn">Espace personnel</h3>
+        <div class="mb-3">
+        <input type="tel" pattern="^((\+\d{1,3}(-| )?\(?\d\)?(-| )?\d{1,5})|(\(?\d{2,6}\)?))(-| )?(\d{3,4})(-| )?(\d{4})(( x| ext)\d{1,5}){0,1}$" placeholder="numero téléphone" id="username">
+        </div>
+        <div class="mb-3">
+        <input type="password" placeholder="Password" id="password">
+</div>
+<button type="submit" class="btn btn-primary">connexion</button>
+    </form>
 </div>
