492d1a2a4e57b3cfaccd8c2298cec3bb7992e8a7
taip6494 <palisseauthomas@gmail.com>
2022-02-22 08:03:45 +0400

    version 13

---
+++ entreprise/vues/ajout_point/form_ajout_point.php
@@ -0,0 +1,38 @@
+<?php
+include("C:/xampp/htdocs/fidelizaou/modules/connexiondb/connexiondb.php");
+//include("C:/xampp/htdocs/fidelizaou/entreprise/modules/ajout_client/add_client.php");
+?>
+<!-- Formulaire ajout client -->
+<?= @$msg ?>
+<?= @$msg_existe ?>
+
+<form action="http://localhost/fidelizaou/entreprise/modules/ajout_client/add_client.php" method="POST" >
+        <select class="form-select" name="Civilite">
+            <option selected>Genre</option>
+            <option value="Monsieur">Monsieur</option>
+            <option value="Madame">Madame</option>
+            <option value="Autre">Autre</option>
+        </select>
+</br>
+        <div class="mb-3">
+            <input type="text" class="form-control" name="Nom" placeholder="Nom">
+        </div>
+        <div class="mb-3">
+            <input type="text" class="form-control" name="Prenom"  placeholder="Prenom">
+        </div>
+        <div class="mb-3">
+            <input type="email" class="form-control" name="Adresse_mail" placeholder="Adresse-mail">
+        </div>
+        <div class="mb-3">
+            <input type="text" class="form-control" name="Numero_tel" placeholder="Numero de téléphone">
+        </div>
+        <div class="mb-3">
+            <input type="date" class="form-control" name="Date_naissance" placeholder="Date de naissance">
+        </div>
+        <div class="mb-3">
+            <input type="text" class="form-control"  name="Code_postal"  placeholder="Code Postal">
+        </div>
+        
+      <button type="submit" class="btn btn-primary" name="submitBtnAjout">Ajouter</button>
+        
+      </form>
